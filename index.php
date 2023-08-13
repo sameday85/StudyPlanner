21a7562b67132a070006f7baec83e439b1471f2f
sameday85 <42813927+sameday85@users.noreply.github.com>
2023-08-13 10:12:53 -0400

    Testing

--- index.php
+++ index.php
@@ -116,7 +116,8 @@ $(document).ready(function() {
         <p>Please use reasonable inputs, or else weird suggestions will be returned.
         <br> Latest update: 7/26/23. Made by Team 15: Victor, Jeffery, Aric, and Bryan.
         <br> Copyright 2023: Study Planner™
-        <br> Aric is still testing this please I need kiwi</p>
+        <br> Aric is still testing this please I need kiwi
+        <br> Testing </p>
     </div>
 </div    
 </body>
